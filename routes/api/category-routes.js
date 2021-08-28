e2099ac7bd3a537ac4fd3bd40f884114b86a574e
Afsha <afsha@Afshas-MacBook-Pro.local>
2021-08-28 17:07:36 +1000

    Basic routing done for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,25 +3,70 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const allData = await Category.findAll({
+      include: [{ model: Product}]
+    });
+    res.status(200).json(allData);
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const byID = await Category.findByPk(req.params.id);
+    res.status(200).json(byID);
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const addCategory = await Category.create(req.body);
+    res.status(200).json(addCategory);
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(updateCategory);
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
+}
   // update a category by its `id` value
-});
+);
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteID = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
   // delete a category by its `id` value
 });
 
